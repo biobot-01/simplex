a8d3eda86dddfe43deb1aaf86ecc584ac9bcbfb6
Kevin Michael <biobot.mike@gmail.com>
2018-07-17 16:34:29 +0200

    Add gulp eslint plugin, fix errors in js plugin file, update package file with gulp eslint and add eslint config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,50 @@
+/* eslint-env node */
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+// Pull in gulp task modules
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
 
-gulp.task('defult', function() {
-  console.log('default gulp tasks');
+// Static server
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: './'
+    });
 });
 
-gulp.task('styles', function() {
-  gulp.src('assets/sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('assets/css'));
+// Compile sass into CSS and auto-inject into browsers
+gulp.task('styles', () => {
+    return gulp.src('assets/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
 });
+
+// Lint all js files
+gulp.task('lint', () => {
+    return gulp.src(['assets/js/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+// Watch for file changes
+gulp.task('watch', ['styles'], () => {
+    gulp.watch('assets/sass/**/*.scss', ['styles']);
+    gulp.watch('assets/js/*.js', ['lint']);
+    gulp.watch('*.html', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'styles', 'lint', 'watch']);
