e4f3f00f225e3935e9c001fed0519fbe070482a1
Kevin Michael <biobot.mike@gmail.com>
2018-07-19 02:34:45 +0200

    Update eslintrc file and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,101 +18,101 @@ const pngquant = require('imagemin-pngquant');
 
 // Static server
 gulp.task('browser-sync', () => {
-    browserSync.init({
-        server: './'
-    });
+  browserSync.init({
+    server: './'
+  });
 });
 
 // Compile sass into CSS and auto-inject into browsers
 gulp.task('styles', () => {
-    return gulp.src('assets/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(browserSync.stream());
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('styles-dist', () => {
-    const plugins = [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        })
-    ];
-    return gulp.src('assets/sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(plugins))
-        .pipe(sourcemaps.write('main.css.map'))
-        .pipe(gulp.dest('assets/css'));
+  const plugins = [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ];
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('main.css.map'))
+    .pipe(gulp.dest('assets/css'));
 });
 
 // Concat all js files
 gulp.task('scripts', () => {
-    return gulp.src('assests/js/*.js')
-        .pipe(babel())
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src('assests/js/*.js')
+    .pipe(babel())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('scripts-dist', () => {
-    return gulp.src('assests/js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat('main.js'))
-        .pipe(uglify('main.min.js'))
-        .pipe(sourcemaps.write('main.js.map'))
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src('assests/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('main.js'))
+    .pipe(uglify('main.min.js'))
+    .pipe(sourcemaps.write('main.js.map'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 // Compress and optimize  images
 gulp.task('images', () => {
-    return gulp.src('assets/img/*')
-        .pipe(imagemin([
-            imagemin.gifsicle({
-                interlaced: true
-            }),
-            imagemin.jpegtran({
-                progressive: true
-            }),
-            imagemin.optipng({
-                optimizationLevel: 5
-            }),
-            imagemin.svgo({
-                removeViewBox: true
-            })
-        ],
-        {use: [pngquant()]}
-        ))
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src('assets/img/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        removeViewBox: true
+      })
+    ],
+    {use: [pngquant()]}
+    ))
+    .pipe(gulp.dest('dist/img'));
 });
 
 // Copy images from dev to production
 gulp.task('copy-images', () => {
-    return gulp.src('assets/img/*')
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src('assets/img/*')
+    .pipe(gulp.dest('dist/img'));
 });
 
 // Lint all js files
 gulp.task('lint', () => {
-    return gulp.src(['assets/js/*.js'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
+  return gulp.src(['assets/js/*.js'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
 });
 
 // Watch for file changes
 gulp.task('watch', ['browser-sync', 'styles', 'scripts', 'lint'], () => {
-    gulp.watch('assets/sass/**/*.scss', ['styles']);
-    gulp.watch('assets/js/*.js', ['lint']);
-    gulp.watch('*.html', browserSync.reload);
+  gulp.watch('assets/sass/**/*.scss', ['styles']);
+  gulp.watch('assets/js/*.js', ['lint']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['browser-sync', 'styles', 'scripts', 'lint', 'watch']);
